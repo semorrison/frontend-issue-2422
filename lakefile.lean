fa85d89e81dccbcc87347a3a4b9fe3f96c207862
Scott Morrison <scott.morrison@gmail.com>
2023-08-15 14:33:07 +1000

    demo of issue

--- lakefile.lean
+++ lakefile.lean
@@ -6,7 +6,12 @@ package frontend
 @[default_target]
 lean_lib Frontend
 
+lean_exe frontend_with_import2 {
+  root := `Frontend.Main_with_Import2
+  supportInterpreter := true
+}
+
 lean_exe frontend {
-  root := `Main
+  root := `Frontend.Main
   supportInterpreter := true
 }
--- test.sh
+++ test.sh
@@ -5,11 +5,23 @@ lake build
 
 # Check that we can compile a file which shares with the executable
 # a common import using an initializer.
-# Here the executable for `frontend` imports `Frontend.Import2`.
+# Here the executable for `frontend_with_import1` imports `Frontend.Import2`.
 
 # This is a minimisation of a situation in which we want to compile a file
 # from a project (e.g. Mathlib), so that we can inject another tactic
 # implemented in the same project into a goal state from the file.
 
-# We observe that this works fine, but fails in the interpreter.
-lake exe frontend Frontend.Import1
+# This works with Lean 4 `master`
+lake exe frontend_with_import2 Frontend.Import1 &&
+
+# Check that if we don't import `Frontend.Import2`, we can successfully run
+#   #eval main ["Frontend.Import1"]
+# in the interpreter
+
+# This works with Lean 4 `master`
+lake build Frontend.Main_with_eval &&
+
+# However if `Main` has imported `Frontend.Import2`, then
+#   #eval main ["Frontend.Import1"]
+# fails to compiled `Frontend.Import1` in the interpreter:
+lake build Frontend.Main_with_Import2_and_eval
